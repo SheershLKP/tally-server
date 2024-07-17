26905095636331adbae3aa65ae90071dfc6a960f
SheershL <sheersh@livekeeping.com>
2024-07-17 15:39:22 +0530

    local ip hit

--- app.js
+++ app.js
@@ -53,14 +53,14 @@ function getLocalIpAddress() {
 app.use(
   '/tally',
   createProxyMiddleware({
-
-    target: `http://${getLocalIpAddress()}:9001`, // Set target to user's local IP address
+    target: 'http://localhost:9001',
+    // target: `http://${getLocalIpAddress()}:9001`, // Set target to user's local IP address
     // target: `http://127.0.0.1:9001`, // Set target to user's local IP address
     headers: {
       "Connection": "keep-alive"
     },
     logLevel: "debug",
-    agent: new HttpsProxyAgent(`http://${getLocalIpAddress()}:9001`),
+    // agent: new HttpsProxyAgent(`http://${getLocalIpAddress()}:9001`),
 
     secure: false,
     changeOrigin: true,
