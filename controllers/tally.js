1e1e265b52b9f7e50aa5c31112a6a53426963a27
SheershL <sheersh@livekeeping.com>
2024-07-18 11:29:24 +0530

    local ip hit

--- controllers/tally.js
+++ controllers/tally.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const callTallyApi = async (req, res) => {
     try {
-        const response = await axios.get('http://127.0.0.1:9001');
+        const response = await axios.get('https://f79b-121-241-109-219.ngrok-free.app/');
         res.status(200).json({ data: response.data, message: "Data found successfully", status: res.status });
     } catch (error) {
         res.status(500).json({ message: "Error fetching data", error: error.message });
